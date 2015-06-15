e018e4d060fbb961444e0c6eaa2ae70d885bffa5
Gillian Basso <gillian.basso@hevs.ch>
2015-06-15 09:05:05 +0200

    update home config

--- semsim/main.py
+++ semsim/main.py
@@ -9,28 +9,50 @@ from sumrecorder import SumRecorder
 
 if __name__ == '__main__':
 
-    for i in range(1, 101):
+    for i in range(1, 2):
 
-        f = '../resources/demo/init/home_%03d.json' % i
-        # f = '../resources/demo/init/home_all.json'
+        house_type = '%03d' % i
+        house_type = "all"
+
+        f = '../resources/demo/init/home_'+house_type+'.json'
+
+        output_dir = "../output/"+house_type+"/"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
         print "Loading file "+str(f)
 
         reader = ScenarioReader(JSONInterpreter(f))
 
-        temp = PlotRecorder('temperature', units.hour, units.degC)
+        temp = PlotRecorder('temperature', y_unit=units.degC)
         reader.simulator.record(temp, reader.simulator.thermal.find(has_attribute='temperature'))
 
         # Create a plot recorder that records the power used by the electrical heater.
-        power = SumRecorder('power', units.hours, units.kilowatt)
-        reader.simulator.record(power, reader.simulator.electrical.find(has_attribute='power'))
+        power = SumRecorder('power')
 
-        c = PlotRecorder('power', units.hour, units.watt)
+        devices = reader.simulator.electrical.find(has_attribute='power')
+        devices = [x for x in devices if not x.friendly_name.startswith('boiler')]
+        devices = [x for x in devices if not x.friendly_name.startswith('heater')]
+
+        print devices
+
+        reader.simulator.record(power, devices)
+
+        # boiler
+
+        c = PlotRecorder('power')
         reader.simulator.record(c, reader.simulator.electrical.find(friendly_name='boiler_%03d' % i))
-        t = PlotRecorder('temperature', units.hour, units.degC)
+        t = PlotRecorder('temperature')
         reader.simulator.record(t, reader.simulator.electrical.find(friendly_name='boiler_%03d' % i))
-        on = PlotRecorder('on', units.hour, bool)
+        on = PlotRecorder('on')
         reader.simulator.record(on, reader.simulator.electrical.find(friendly_name='boiler_%03d' % i))
+
+        # heat pump
+
+        c_hp = PlotRecorder('power')
+        reader.simulator.record(c_hp, reader.simulator.electrical.find(friendly_name='heater_%03d' % i))
+        on_hp = PlotRecorder('on')
+        reader.simulator.record(on_hp, reader.simulator.electrical.find(friendly_name='heater_%03d' % i))
 
         print("Running simulation...")
 
@@ -42,15 +64,25 @@ if __name__ == '__main__':
 
         # Create a PD document, add the two figures of the plot recorder to the
         # document and close the document.
-        FigureSaver(temp, "Temperature").save('../output/demo-temp_%03d.pdf' % i)
-        FigureSaver(power, "Power").save('../output/demo-power_%03d.pdf' % i)
+        FigureSaver(temp, "Temperature").save(output_dir+'temp.pdf')
+        FigureSaver(power, "Power").save(output_dir+'power.pdf')
 
-        FigureSaver(c, "Power").save('../output/demo-boiler-power_%03d.pdf' % i)
-        CSVSaver(c).save('../output/demo-boiler-power_%03d.csv' % i)
-        FigureSaver(t, "Temperature").save('../output/demo-boiler-temp_%03d.pdf' % i)
-        CSVSaver(t).save('../output/demo-boiler-temp_%03d.csv' % i)
-        FigureSaver(on, "Control").save('../output/demo-boiler-control_%03d.pdf' % i)
-        CSVSaver(on).save('../output/demo-boiler-control_%03d.csv' % i)
+        boiler_dir = output_dir+'boiler/'
+        if not os.path.exists(boiler_dir):
+            os.makedirs(boiler_dir)
 
-        # FigureSaver(temp, "Temperature").save('../output/demo-temp_all.pdf')
-        # FigureSaver(power, "Power").save('../output/demo-power_all.pdf')$
+        FigureSaver(c, "Power").save(boiler_dir+'power.pdf')
+        CSVSaver(c).save(boiler_dir+'power.csv')
+        FigureSaver(t, "Temperature").save(boiler_dir+'temp.pdf')
+        CSVSaver(t).save(boiler_dir+'temp.csv')
+        FigureSaver(on, "Control").save(boiler_dir+'control.pdf')
+        CSVSaver(on).save(boiler_dir+'control.csv')
+
+        hp_dir = output_dir+'hp/'
+        if not os.path.exists(hp_dir):
+            os.makedirs(hp_dir)
+
+        FigureSaver(on_hp, "Control").save(hp_dir+'control.pdf')
+        CSVSaver(on_hp).save(hp_dir+'control.csv')
+        FigureSaver(c_hp, "Power").save(hp_dir+'power.pdf')
+        CSVSaver(c_hp).save(hp_dir+'power.csv')
